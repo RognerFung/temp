ac59760f076e40441f54bede791b7617c8fdda13
Rogner <fangxiaoyong00@gmail.com>
2018-09-06 23:28:55 +0800

    Assignment 3

--- routes/users.js
+++ routes/users.js
@@ -7,38 +7,47 @@ var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.route('/')
+  .get(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) =>  {
+    User.find({})
+    .then((users) => {
+      res.statusCode = 200;
+      res.setHeader('Content-Type', 'application/json');
+      res.json(users);
+    }, (err) => next(err))
+    .catch((err) => next(err));
+  }
+);
 
 router.post('/signup', (req, res, next) => {
   User.register(new User({username: req.body.username}), 
     req.body.password, (err, user) => {
-    if(err) {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({err: err});
-    }
-    else {
-      if (req.body.firstname)
-        user.firstname = req.body.firstname;
-      if (req.body.lastname)
-        user.lastname = req.body.lastname;
-      user.save((err, user) => {
-        if (err) {
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({err: err});
-          return ;
-        }
-        passport.authenticate('local')(req, res, () => {
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'application/json');
-          res.json({success: true, status: 'Registration Successful!'});
+      if(err) {
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'application/json');
+        res.json({err: err});
+      }
+      else {
+        if (req.body.firstname)
+          user.firstname = req.body.firstname;
+        if (req.body.lastname)
+          user.lastname = req.body.lastname;
+        user.save((err, user) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({err: err});
+            return ;
+          }
+          passport.authenticate('local')(req, res, () => {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({success: true, status: 'Registration Successful!'});
+          });
         });
-      });
+      }
     }
-  });
+  );
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
@@ -62,6 +71,3 @@ router.get('/logout', (req, res) => {
 });
 
 module.exports = router;
-
-//5b8571fe6b63bd5280dd4b5b
-//eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Yjg1NzAzNTZiNjNiZDUyODBkZDRiNTkiLCJpYXQiOjE1MzU0NzE3MTMsImV4cCI6MTUzNTQ3NTMxM30.AmukXkQY4PAQxoe_DEvNIKWYGqF1BGEfdJSHX7jPssw
