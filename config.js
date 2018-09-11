33287f6e3fa52f1fc796bc43b5c0471b0343a06b
Rogner <fangxiaoyong00@gmail.com>
2018-09-11 09:26:08 +0800

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '1675473695894588',
+        clientSecret: '633544c3d5832ab16b4a41a65b1ad378'
+    }
 }
